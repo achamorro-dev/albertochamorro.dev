2daed2e91ecc35cce3661f974f8d1c8162485907
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2021-04-19 06:21:36 +0200

    :sparkles: Added empty pages and nuxt-links

---
+++ components/ui/shared/Link.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="href" target="_blank" class="link">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  name: 'Link',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.link {
+  @apply cursor-pointer text-custom-blue no-underline;
+}
+</style>
