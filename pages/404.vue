49b1ce667a9ea80a723ab84d4ea59c1f6b7e7aca
Alberto Chamorro <achamorro@vivelibre.es>
2021-07-12 16:17:05 +0200

    :lipstick: Fixed styles in 404 page

--- pages/404.vue
+++ pages/404.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="xl-container h-screen text-center">
+    <h2 class="text-2xl mt-12">Oops! Algo ha salido mal…</h2>
     <img src="@/assets/images/error.png" class="m-auto" />
-    <h2>Oops! Algo ha salido mal…</h2>
+    <Button class="mt-4 mx-auto" @btn-clicked="goHome">llévame a casa</Button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.push({ name: 'index' })
+    },
+  },
+}
+</script>
