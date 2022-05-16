f133a703c55ecbb109036b4648f7d4ce97c97834
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2022-05-16 10:27:04 +0200

    fix: wrong line in 404 page

--- pages/404.vue
+++ pages/404.vue
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="../../../../Downloads/cera-round-pro-cufonfonts-webfont/style.css">
 <template>
   <div class="xl-container h-screen text-center">
     <h2 class="text-2xl mt-12">
