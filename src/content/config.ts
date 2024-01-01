0e486390f3d71551dfe45f89b315ac54e1a40e88
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-01 20:41:00 +0100

    feat(blog): remove metaImg property and use the image zod type for cover

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,16 @@ import { defineCollection, z } from 'astro:content'
 
 const blogTags = z.enum(['javascript', 'typescript', 'frontend', 'backend', 'productividad', 'flutter', 'git'])
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // TODO
-    cover: z.string(),
-    metaImg: z.string(),
-    alt: z.string(),
-    // TODO
-    tags: z.array(blogTags),
-    date: z.date(),
-    canonicalUrl: z.string().url(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      cover: image(),
+      alt: z.string(),
+      tags: z.array(blogTags),
+      date: z.date(),
+      canonicalUrl: z.string().url(),
+    }),
 })
 
 export const collections = { blog }
