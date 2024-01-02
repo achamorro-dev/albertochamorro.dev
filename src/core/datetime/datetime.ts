368e448a6e7d4a386a9a9aa6ae8de72b4bdfb753
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-02 10:32:03 +0100

    feat: change date style

--- src/core/datetime/datetime.ts
+++ src/core/datetime/datetime.ts
@@ -1,7 +1,7 @@
 export class Datetime {
   static toHumanDate(date: Date): string {
     return new Intl.DateTimeFormat('es-ES', {
-      dateStyle: 'medium',
+      dateStyle: 'long',
     }).format(date)
   }
 }
