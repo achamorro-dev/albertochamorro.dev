c73abb5b13e5f6e5d6eb1ef7e7d1917333f4a5e4
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-01-03 11:09:22 +0100

    feat: add robots.txt generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://albertochamorro.dev',
-  integrations: [mdx(), sitemap(), react()]
-});
+  integrations: [mdx(), sitemap(), react(), robotsTxt()],
+})
