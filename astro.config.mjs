e2c422ee06cc480de3dfcd36162fe870ab5234e0
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-03-31 17:46:23 +0200

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,23 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
 import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://albertochamorro.dev',
-  integrations: [mdx(), sitemap(), react(), robotsTxt()],
+  output: 'server',
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    robotsTxt(),
+    vercel({
+      webAnalytics: {
+        enabled: true,
+      },
+    }),
+  ],
 })
