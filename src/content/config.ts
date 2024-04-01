8a3403449bed754fe96e81f6169704d5cda24481
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-04-01 07:18:35 +0200

    feat: add blog post "Especificidad en CSS, ¿qué es y cómo se calcula?"

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from 'astro:content'
 
-const blogTags = z.enum(['javascript', 'typescript', 'frontend', 'backend', 'productividad', 'flutter', 'git'])
+const blogTags = z.enum(['javascript', 'typescript', 'frontend', 'backend', 'productividad', 'flutter', 'git', 'css'])
 const blog = defineCollection({
   type: 'content',
   schema: ({ image }) =>
@@ -12,6 +12,18 @@ const blog = defineCollection({
       tags: z.array(blogTags),
       date: z.date(),
       canonicalUrl: z.string().url(),
+    }),
+})
+
+const work = defineCollection({
+  type: 'data',
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      period: z.string(),
+      location: z.string(),
+      description: z.string(),
+      logo: image(),
     }),
 })
 
