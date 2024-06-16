f4b1f6f2e1905d4a2fb0ce6d4bfef3389be703e9
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2024-06-16 10:19:13 +0200

    feat: add shiki transformers for notation diffs, notation focus and highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import vercel from '@astrojs/vercel/serverless'
+import { transformerMetaHighlight, transformerNotationDiff, transformerNotationFocus } from '@shikijs/transformers'
+import { defineConfig } from 'astro/config'
 
 import robotsTxt from 'astro-robots-txt'
 
@@ -10,6 +11,13 @@ import robotsTxt from 'astro-robots-txt'
 export default defineConfig({
   site: 'https://albertochamorro.dev',
   output: 'server',
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'catppuccin-mocha',
+      transformers: [transformerNotationDiff(), transformerNotationFocus(), transformerMetaHighlight()],
+    },
+  },
   integrations: [
     mdx(),
     sitemap(),
